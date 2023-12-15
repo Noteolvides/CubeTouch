7b3e69fa119682303aba3824b0f87b30bef4c764
noteolvides <gustavogomezlopezdesanroman@gmail.com>
2023-12-15 01:59:26 +0100

    refactor: improve touch code

--- Examples/CustomFirmware/Touch.c
+++ Examples/CustomFirmware/Touch.c
@@ -1,7 +1,5 @@
 #include "Mapping.h"
-#define TouchKeyQueryCyl2ms() {TKEY_CTRL |= bTKC_2MS;}                         //Touch button sampling period setting 2ms
-
-#define KEY_ACT              2500
+#include <TouchKey.h>
 
 enum states {
   RELEASED,
@@ -20,46 +18,54 @@ __xdata uint8_t idleTouchValue[5];
 #define DELAY 10
 #define LOONG_PRESS 2000
 
-
-void touchBegin(){
-  delay(1000);
-  for (uint8_t i = 0; i < 5; i++) {
-      TKEY_CTRL = TKEY_CTRL & 0xF8 | (2 + i);
-      while((TKEY_CTRL&bTKC_IF) == 0);
-      idleTouchValue[i] = TKEY_DAT;
-  }
-
-  P1_DIR_PU &= 0x0C;
-  TouchKeyQueryCyl2ms();
-}
-
 void initTouch() {
-  touchBegin();
   for (uint8_t i = 0; i < 5; i++)
   {
     stateButtons[i].history = RELEASED;
     stateButtons[i].downTime = 0;
   }
+
+    TouchKey_begin( (1 << 1) | (1 << 2) | (1 << 3) | (1 << 4) | (1 << 5) ); //Enable all 6 channels: TIN0(P1.0), TIN1(P1.1), TIN2(P1.4), TIN3(P1.5), TIN4(P1.6), TIN5(P1.7)
+    //refer to AN3891 MPR121 Baseline System for more details
+    TouchKey_SetMaxHalfDelta(5);      //increase if sensor value are more noisy
+    TouchKey_SetNoiseHalfDelta(2);    //If baseline need to adjust at higher rate, increase this value
+    TouchKey_SetNoiseCountLimit(10);  //If baseline need to adjust faster, increase this value
+    TouchKey_SetFilterDelayLimit(5);  //make overall adjustment slopwer
+
+    TouchKey_SetTouchThreshold(100);  //Bigger touch pad can use a bigger value
+    TouchKey_SetReleaseThreshold(80); //Smaller than touch threshold
 }
 
 unsigned long lastTimeTouch;
 void runTouch(unsigned long actualTime) {
-  if (actualTime - lastTimeTouch > 9) {
+  if (actualTime - lastTimeTouch >= DELAY) {
     lastTimeTouch = actualTime;
+    TouchKey_Process();
+    uint8_t touchResult = TouchKey_Get();
     for (uint8_t i = 0; i < 5; i++) {
-      TKEY_CTRL = TKEY_CTRL & 0xF8 | (2 + i);
-      while((TKEY_CTRL&bTKC_IF) == 0);                                          
-      int16_t touchResult = TKEY_DAT;
-      touchResult=(abs(idleTouchValue[i]-touchResult)>KEY_ACT);
+      bool active = touchResult & (1 << (i+1));
+      USBSerial_print("TIN");
+      USBSerial_print(i);
+      if (stateButtons[i].history == RELEASED) {
+        USBSerial_print(" _ ");
+      } 
+      if (stateButtons[i].history == HOLDING) {
+        USBSerial_print(" X ");
+      } 
+      if (stateButtons[i].history == LONG_HOLD) {
+        USBSerial_print(" Y ");
+      } 
       switch (stateButtons[i].history) {
         case RELEASED:  // Not presed before
-          if (touchResult)
+          if (!active)
             break;  // Not touched
-          stateButtons[i].history = HOLDING;  // Touch detected
           stateButtons[i].downTime += DELAY;
+          if(stateButtons[i].downTime>=DELAY*5){
+            stateButtons[i].history = HOLDING;  // Touch detected
+          }
           break;
         case HOLDING:                       // Touch detected before
-          if (touchResult) {  // Is now relesed
+          if (!active) {  // Is now relesed
             stateButtons[i].downTime = 0;
             stateButtons[i].history = RELEASED;
             dataKeys[i].simpleTouchFunction(i);
@@ -74,11 +80,12 @@ void runTouch(unsigned long actualTime) {
           }
           break;
         case LONG_HOLD:
-          if (touchResult) {
+          if (!active) {
             stateButtons[i].history = RELEASED;  // Have to wait to long press is released
           }
           break;
       }
     }
+    USBSerial_println();
   }
 }
--- Examples/CustomFirmware/Touch.h
+++ Examples/CustomFirmware/Touch.h
@@ -3,7 +3,6 @@
 
 #include <stdint.h>
 #include <Arduino.h>
-#include <TouchKey.h>
 
 
 void initTouch();
